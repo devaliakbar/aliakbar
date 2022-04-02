81b8b6a3abc1e589fe034543e2451d08254829dd
Ali <dev.aliakbar@gmail.com>
2022-04-02 10:33:04 +0530

    Home Design#1

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -1,6 +1,8 @@
 part of 'app_theme.dart';
 
 class AppColor {
-  static const Color background = Color(0xff111111);
+  static const Color background = Color(0xff1d1d1d);
   static const Color grey = Color(0xff777777);
+  static const Color greyDark = Color(0xff181818);
+  static const Color textColor = Color(0xff21fdd8);
 }
--- lib/features/home/presentation/screen/home.dart
+++ lib/features/home/presentation/screen/home.dart
@@ -1,6 +1,7 @@
 import 'package:aliakbar/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
@@ -16,33 +17,305 @@ class Home extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: _screenUtil.setWidth(40),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
               ),
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Row(
                 children: [
-                  Text(
-                    "Ali",
-                    style: GoogleFonts.bebasNeue(
-                      textStyle: TextStyle(
-                        color: AppColor.grey,
-                        fontSize: _screenUtil.setSp(170),
-                      ),
+                  Container(
+                    width: _screenUtil.setWidth(60),
+                    margin: EdgeInsets.only(
+                      right: _screenUtil.setWidth(40),
+                    ),
+                    height: MediaQuery.of(context).size.height,
+                    color: AppColor.greyDark,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black,
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(
+                                  top: _screenUtil.setHeight(50),
+                                  left: _screenUtil.setWidth(15),
+                                  right: _screenUtil.setWidth(15),
+                                ),
+                                child:
+                                    Image.asset("assets/images/ali_icon.png"),
+                              ),
+                              SizedBox(
+                                height: _screenUtil.setHeight(10),
+                              ),
+                              Text(
+                                "Full Stack Developer",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(6),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: _screenUtil.setHeight(30),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  top: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "About",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Skills",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Works",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Education",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Contact",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                vertical: _screenUtil.setHeight(25),
+                              ),
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: AppColor.grey,
+                                    width: _screenUtil.setHeight(0.5),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Blog",
+                                style: GoogleFonts.roboto(
+                                  textStyle: TextStyle(
+                                    color: AppColor.grey,
+                                    fontSize: _screenUtil.setSp(9),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            bottom: _screenUtil.setHeight(40),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              SvgPicture.asset(
+                                "assets/images/linkedin.svg",
+                                width: _screenUtil.setWidth(7.5),
+                                color: AppColor.grey,
+                              ),
+                              SvgPicture.asset(
+                                "assets/images/github.svg",
+                                width: _screenUtil.setWidth(7.5),
+                                color: AppColor.grey,
+                              ),
+                              SvgPicture.asset(
+                                "assets/images/instagram.svg",
+                                width: _screenUtil.setWidth(7.5),
+                                color: AppColor.grey,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Text(
-                    "Akbar",
-                    style: GoogleFonts.bebasNeue(
-                      textStyle: TextStyle(
-                        color: AppColor.grey,
-                        fontSize: _screenUtil.setSp(170),
-                      ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Ali",
+                          style: GoogleFonts.bebasNeue(
+                            textStyle: TextStyle(
+                              color: AppColor.grey,
+                              fontSize: _screenUtil.setSp(170),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Akbar",
+                              style: GoogleFonts.bebasNeue(
+                                textStyle: TextStyle(
+                                  color: AppColor.grey,
+                                  fontSize: _screenUtil.setSp(170),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  right: _screenUtil.setWidth(40)),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Full Stack Developer",
+                                    style: GoogleFonts.roboto(
+                                      textStyle: TextStyle(
+                                          color: AppColor.textColor,
+                                          fontSize: _screenUtil.setSp(8),
+                                          letterSpacing: 3),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      top: _screenUtil.setHeight(30),
+                                    ),
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: _screenUtil.setWidth(15),
+                                      vertical: _screenUtil.setWidth(7),
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: AppColor.textColor,
+                                        width: _screenUtil.setHeight(1.5),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      "Contact me!",
+                                      style: GoogleFonts.roboto(
+                                        textStyle: TextStyle(
+                                            color: AppColor.textColor,
+                                            fontSize: _screenUtil.setSp(8),
+                                            letterSpacing: 3),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
