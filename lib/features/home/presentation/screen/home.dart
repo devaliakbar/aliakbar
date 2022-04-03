40f37e5d3e75ae66c06372df371c466ed770e748
Ali <dev.aliakbar@gmail.com>
2022-04-03 15:21:06 +0530

    Refactored

--- lib/features/home/presentation/screen/home.dart
+++ lib/features/home/presentation/screen/home.dart
@@ -3,30 +3,8 @@ import 'package:aliakbar/features/home/presentation/widgets/landing/landing.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final ValueNotifier<bool> _initialLoading = ValueNotifier(true);
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(
-      const Duration(milliseconds: 1500),
-    ).then((value) => _initialLoading.value = false);
-  }
-
-  @override
-  void dispose() {
-    _initialLoading.dispose();
-    super.dispose();
-  }
 
   void onInitialAnimationDone() {}
 
@@ -49,19 +27,8 @@ class _HomeState extends State<Home> {
                 minHeight: MediaQuery.of(context).size.height -
                     _screenUtil.setWidth(40),
               ),
-              child: ValueListenableBuilder<bool>(
-                valueListenable: _initialLoading,
-                builder:
-                    (BuildContext context, bool initialLoading, Widget? child) {
-                  if (initialLoading) {
-                    return const SizedBox.shrink();
-                  }
-
-                  ///BODY
-                  return Landing(
-                    onInitialAnimationDone: onInitialAnimationDone,
-                  );
-                },
+              child: Landing(
+                onInitialAnimationDone: onInitialAnimationDone,
               ),
             ),
           ),
